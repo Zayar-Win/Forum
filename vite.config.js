98d25bc1ed700d3f3d839529b7284244bda91395
ROHS-7 <zwin7748@gmail.com>
2026-01-02 22:19:27 +0700

    vite-config-change

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+function getPages(dir, files = []) {
+    fs.readdirSync(dir).forEach(file => {
+      const full = path.join(dir, file)
+      if (fs.statSync(full).isDirectory()) getPages(full, files)
+      else if (file.endsWith('.vue')) files.push(full)
+    })
+    return files
+  }
+  
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/js/app.js',
+                ...getPages('resources/js/Pages')   
+              ],
             refresh: true,
         }),
         vue({
